79e6003078e2c346ae3fea9616e16d7b158a60e2
FPierre <pflauder@gmail.com>
2015-05-27 21:53:10 +0200

    Suppression des fichiers tokens.h et tokens.cpp (inutilises)

--- headers/tokens.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOKENS_H
-#define TOKENS_H
-
-#include <vector>
-#include "Document.h"
-#include "Token.h"
-
-
-#endif
--- sources/tokens.cpp
+++
@@ -1,5 +0,0 @@
-#include <boost/lexical_cast.hpp>
-#include <boost/regex.hpp>
-#include <boost/unordered_set.hpp>
-#include "tokens.h"
-
